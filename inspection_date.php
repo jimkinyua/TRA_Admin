a0840d72c326d305739f6bc751b989f29b6387cc
jimkinyua <jimkinyua25@gmail.com>
2020-12-14 16:32:41 +0300

    okay

--- inspection_date.php
+++ inspection_date.php
@@ -274,3 +274,7 @@ $(document).ready(function(){
 
       </fieldset>
   </form>                  
+<script>
+var today = new Date().toISOString().split('T')[0];
+document.getElementsByName("SetDate")[0].setAttribute('min', today);
+</script>
